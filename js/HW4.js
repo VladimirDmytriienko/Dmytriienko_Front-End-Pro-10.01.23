5fadcdb2e59a86aa1372075908f608d59d69d024
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-01-24 14:59:43 +0200

    009 Commit HW 10 11 fix variables naming

--- js/HW4.js
+++ js/HW4.js
@@ -1,140 +1,139 @@
-// let b = "";
-// for (let i = 10; i <= 20; i++) {
-//   b += i+ ",";
-// }
-// console.log(b);
+let b = "";
+for (let i = 10; i <= 20; i++) {
+  b += i+ ",";
+}
+console.log(b);
 
-// let b;
-// for (let i = 10; i <= 20; i++) {
-//     b = i ** 2;
-//     console.log(b);
-// }
+let a;
+for (let i = 10; i <= 20; i++) {
+    a = i ** 2;
+    console.log(a);
+}
 
-// for (let i = 1; i<=7; i++) { 
-//     console.log(i + "*" + '7' + "=" + i*7 );
-// }
+for (let i = 1; i<=7; i++) { 
+    console.log(i + "*" + '7' + "=" + i*7 );
+}
 
-// let b = 0;
-// for (let i = 1; i<=15; i++) { 
-//     b += i;
-// }
-// console.log(b);
+let c = 0;
+for (let i = 1; i<=15; i++) { 
+    c += i;
+}
+console.log(c);
 
-// let b = 15;
-// for (let i = 16; i<=35; i++) { 
-//     b = b * i;
-// }
-// console.log(b);
+let d = 15;
+for (let i = 16; i<=35; i++) { 
+    d = d * i;
+}
+console.log(d);
 
-// let b = 0;
-// for (let i = 1; i<=500; i++) { 
-//     b += i/500;
-// }
-// console.log(b);
-
-
-// let b = 30; 
-// for (let i = 30; i <= 80; i++) {
-//     if (i % 2 == 0) {
-//       b += i;
-// }
-
-// }
-// console.log(b);
+let e = 0;
+for (let i = 1; i<=500; i++) { 
+    e += i/500;
+}
+console.log(e);
 
 
-// for (let i = 100; i <= 200; i++) {
-//     if (i % 3 == 0) {
-//       console.log(i);
-//     }
-// }
-
-// let b = 20;
-// for(let i = 0; i <= b; i++) {
-//   if(b % i == 0) {
-//     console.log(i);
-//   }
-// }
+let f = 30; 
+for (let i = 30; i <= 80; i++) {
+    if (i % 2 == 0) {
+      f += i;
+    }
+}
+console.log(f);
 
 
-// let b = 20;
-// let amount = 0;
-// for(let i = 0; i <= b; i++) {
-//   if(b % i == 0) {
-//     if (i % 2 === 0) {
-//       amount++;
-//     }
-//   }
-// }
-// console.log(amount);
+for (let i = 100; i <= 200; i++) {
+    if (i % 3 == 0) {
+      console.log(i);
+    }
+}
 
-// let b = 20;
-// let sum = 0;
-// for(let i = 0; i <= b; i++) {
-//   if(b % i == 0) {
-//     if (i % 2 === 0) {
-//       sum += i;
-//     }
-//   }
-// }
-// console.log(sum);
+let g = 20;
+for(let i = 0; i <= g; i++) {
+  if(g % i == 0) {
+    console.log(i);
+  }
+}
 
 
-// for( let a = 1; a <= 10; a++){
-//   for(let b = 1; b <= 10; b++){
+let h = 20;
+let amount = 0;
+for(let i = 0; i <= h; i++) {
+  if(h % i == 0) {
+    if (i % 2 === 0) {
+      amount++;
+    }
+  }
+}
+console.log(amount);
 
-//     console.log(a + "*" + b + " = " + a*b);
+let k = 20;
+let sum = 0;
+for(let i = 0; i <= k; i++) {
+  if(k % i == 0) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  }
+}
+console.log(sum);
+
+
+for( let a = 1; a <= 10; a++){
+  for(let j = 1; j <= 10; j++){
+
+    console.log(a + "*" + j + " = " + a*j);
   
-//   }
-// }
+  }
+}
 
 ////////////////////////////////////////////////////HW11
 
-// let b = "";
-// for (let i = 20; i <= 30; i += 0.5) {
-//   b += i + " ";
-// }
-// console.log(b);
+let l = "";
+for (let i = 20; i <= 30; i += 0.5) {
+  l += i + " ";
+}
+console.log(l);
 
 
-// let b = "";
-// for (let i = 10; i <= 100; i += 10) {
-//   b += i*27 + " ";
-// }
-// console.log(b);
+let m = "";
+for (let i = 10; i <= 100; i += 10) {
+  m += i*27 + " ";
+}
+console.log(m);
 
-// let b = 50;
-// let c;
-// for (let i = 1; i <= 100; i++) {
-//   c=i**2;
-//   if( c<b ) {
-//     console.log(i);
-//   }
-// }
-
-
-// function primeNumb(num) {
-//   if (num > 1) {
-//     for (let i = 2; i < num; i++) {
-//       if (num % i == 0) {
-//         return "Составное число";
-//       }
-//     }
-//     return "Простое число";
-//   } else {
-//     return "Число должно быть больше 1";
-//   }
-// }
-// console.log(primeNumb(19));
+let n = 50;
+let o;
+for (let i = 1; i <= 100; i++) {
+  o=i**2;
+  if( o<n ) {
+    console.log(i);
+  }
+}
 
 
+function primeNumb(num) {
+  if (num > 1) {
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) {
+        return "Составное число";
+      }
+    }
+    return "Простое число";
+  } else {
+    return "Число должно быть больше 1";
+  }
+}
+console.log(primeNumb(19));
 
-// function isPowerOfThree(num) {
-//   while (num % 3 == 0) {
-//     num /= 3;
-//   }
-//   return num == 1;
-// }
-// console.log(isPowerOfThree(81));
+
+
+function isPowerOfThree(num) {
+  while (num % 3 == 0) {
+    num /= 3;
+  }
+  return num == 1;
+}
+console.log(isPowerOfThree(81));
 
 
