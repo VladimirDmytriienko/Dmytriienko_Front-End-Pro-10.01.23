9cf335eeae670ecfe022f1146f5f4e12fa60c53e
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-02-03 21:58:05 +0200

    012 Hw7 add 16 17

--- js/Hw7.js
+++ js/Hw7.js
@@ -1,11 +1,25 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
-function removeElement () {
-    array.splice(4, 1);
+function removeElement(array, cutNum) {
+    const index = array.indexOf(cutNum);
+    array.splice(index, 1);
+    console.log(array);
 }
-removeElement();
-console.log(array);
+removeElement(array, 5);
 
+////////////////////////////////////////////////////////
 
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+function generateKey(length, characters ) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const key = generateKey(16, characters);
+console.log(key); 
 
