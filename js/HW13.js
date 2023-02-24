639ff08ed70f1f40b0473c951c83c47467227d3b
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-02-24 17:30:31 +0200

    022 commit Hw28 fix variables and third part

--- js/HW13.js
+++ js/HW13.js
@@ -1,21 +1,61 @@
 
 function dayToday(givenNum) {
     const now = new Date().setHours(0, 0, 0, 0);
-    const asd = (givenNum === now) ? true : false;
-    console.log(asd);
+    const isDayToday = (givenNum === now) ? true : false;
+    return isDayToday
 }
-dayToday(1677015800000);
+console.log(dayToday(1677103200000));
 
+/////////////////////////////////////////////////////////////////////////////////////////
 
-function returnDate(mileseconds) {
-    const date = new Date(mileseconds);
-    console.log(date.toString());
+ 
+function returnDate(d){
+    const stringDate = new Date(d)
+	const date = stringDate.getDate().toString().padStart(2, "0");
+	const month = (stringDate.getMonth() + 1).toString().padStart(2, "0");
+	const year = stringDate.getFullYear();
+
+	const h = stringDate.getHours().toString().padStart(2, "0");
+	const min = stringDate.getMinutes().toString().padStart(2, "0");
+
+	return `${date}.${month}.${year}, ${h}:${min}`;
 }
-returnDate(1677015800000);
+
+console.log(returnDate(1577015800000));
+
+//////////////////////////////////////////////////////////////////////
 
 
-function dateEr(yourTime) {
-    const asd = new Date().getTime();
-    console.log (asd == yourTime);
+function dateDiference(yourTime) {
+    const date1 = new Date(yourTime);
+    const date2 = new Date();
+    let diff, willOrWas;
+    
+    if (date2 > date1) {
+        diff = date2.getTime() - date1.getTime();
+        willOrWas = "прошло";
+    
+    } else if(date1 < date2) {
+        diff = date1.getTime() - date2.getTime();
+        willOrWas = "пройдет";
+    }
+
+    let msec = diff;
+    const yy = Math.floor(msec / 12 / 30 / 24 / 1000 / 60 / 60);
+    msec -= yy * 12 * 30 * 24 * 1000 * 60 * 60; 
+    const month = Math.floor(msec / 30  / 24 / 1000 / 60 / 60);
+    msec -= month * 30 * 24 * 1000 * 60 * 60;
+    const dd = Math.floor(msec / 24 / 1000 / 60 / 60);
+    msec -= dd * 24 * 1000 * 60 * 60;
+    const hh = Math.floor(msec / 1000 / 60 / 60);
+    msec -= hh * 1000 * 60 * 60;
+    const mm = Math.floor(msec / 1000 / 60);
+    msec -= mm * 1000 * 60;
+    const ss = Math.floor(msec / 1000);
+    msec -= ss * 1000;
+    
+    return `${willOrWas} ${yy} годов/лет ${month}  месяцов ${dd} дней ${hh} часов ${hh} дней ${mm} минут`
 }
-dateEr(1677015800000);
+
+console.log(dateDiference(1588983680000))
+
