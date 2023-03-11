adbf093c5a8d74e9b01b04229ab68402b9ffc640
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-03-11 16:40:27 +0200

    026 commit Hw 32 Promise

---
+++ js/Hw16.js
@@ -0,0 +1,14 @@
+
+new Promise((resolve, reject) => {
+    console.log('Проснулся');
+    resolve();
+})
+.then(() => {
+    console.log('Почистил зубы');
+})
+.then(() => {
+    console.log('Поел');
+})
+.then(() => {
+    console.log('Пошел учить JS');
+});
