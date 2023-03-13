ca86dcb5bfa198c8fac7437201a1400460426f60
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-03-13 15:07:23 +0200

    030 commit Hw 33 async await

---
+++ js/Hw17.js
@@ -0,0 +1,38 @@
+function* getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    for (let i = 0; i < 10; i++) {
+        yield Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+
+const random = getRandomInt(1, 100);
+for (const number of random) {
+    console.log(number)
+}
+//////////////////////////////////////////////////////////////////////
+
+const getUp = () => {
+    return new Promise((resolve) => setTimeout(() => {resolve('Проснулся')}, 1000))
+}
+const brushTeeth  = () => {
+    return new Promise(r => setTimeout(() => r('Почистили зубы'), 1000))
+}
+const haveBreakfast = () => {
+    return new Promise(r => setTimeout(() => r('Завтрак'), 1000))
+}
+
+const learnJs  = () => {
+    return new Promise(r => setTimeout(() => r('Пошел учить JS'), 1000))
+}
+const day  =  async function  () {
+    const up = await getUp()
+    const teeth = await brushTeeth ()
+    const breakfast = await haveBreakfast()
+    const js = await learnJs()
+    console.log(up, teeth, breakfast, js)
+}
+day()
+
+
+
